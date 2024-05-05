b84782314872516038b0cf0a0842f78bae171342
Mdsakib999 <sakibabrar999@gmail.com>
2024-05-05 11:11:50 +0600

    contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -51,12 +51,12 @@ const Contact = () => {
       </div>
 
       {/* Address part */}
-      <div className="flex flex-col md:flex-row items-center gap-7 lg:gap-0 justify-evenly my-10">
+      <div className=" lg:w-[90%] mx-auto flex flex-col md:flex-row items-center gap-7 lg:gap-0 justify-between my-16">
         
 
 
         <div className="md:w-1/2">
-          <div className="text-2xl font-bold mb-4 ">Get In Touch</div>
+          <div className="text-2xl font-bold mb-6 ">-- Get In Touch --</div>
           <form action="#" className="w-full">
             <div className="flex flex-wrap -mx-2 mb-6">
               <div className="w-full md:w-1/2 px-2 mb-4 md:mb-0">
@@ -64,7 +64,7 @@ const Contact = () => {
                   type="text"
                   placeholder="Name"
                   required
-                  className="w-full h-12 border-2 border-gray-300 rounded px-4 focus:outline-none focus:border-green-500 transition duration-300"
+                  className="w-full h-12 border border-gray-300 rounded px-4 focus:outline-none focus:border-orange-400 transition duration-300"
                 />
               </div>
               <div className="w-full md:w-1/2 px-2">
@@ -72,7 +72,7 @@ const Contact = () => {
                   type="email"
                   placeholder="Email"
                   required
-                  className="w-full h-12 border-2 border-gray-300 rounded px-4 focus:outline-none focus:border-green-500 transition duration-300"
+                  className="w-full h-12 border border-gray-300 rounded px-4 focus:outline-none focus:border-orange-400 transition duration-300"
                 />
               </div>
             </div>
@@ -81,7 +81,7 @@ const Contact = () => {
                 type="text"
                 placeholder="Subject"
                 required
-                className="w-full h-12 border-2 border-gray-300 rounded px-4 focus:outline-none focus:border-green-500 transition duration-300"
+                className="w-full h-12 border border-gray-300 rounded px-4 focus:outline-none  focus:border-orange-400 transition duration-300"
               />
             </div>
             <div className="mb-6">
@@ -90,15 +90,15 @@ const Contact = () => {
                 rows="10"
                 placeholder="Write your Message..."
                 required
-                className="w-full h-32 border-2 border-gray-300 rounded px-4 py-2 focus:outline-none focus:border-green-500  transition duration-300"
+                className="w-full h-32 border border-gray-300 rounded px-4 py-2 focus:outline-none focus:border-orange-400  transition duration-300"
               ></textarea>
             </div>
             <div className="flex items-center">
               <button
                 type="submit"
-                className="group mt-2  border border-green-500 hover:border-none relative h-12 w-44 overflow-hidden rounded-lg font-bold bg-slate-50 text-lg shadow-md"
+                className="group mt-2  border border-orange-400 hover:border-none relative h-12 w-44 overflow-hidden rounded-ss-xl rounded-ee-xl font-bold bg-slate-50 text-lg shadow-md hover:shadow-lg"
               >
-                <div className="absolute inset-0 w-3 bg-gradient-to-r from-green-600 to-lime-300 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+                <div className="absolute inset-0 w-3 bg-gradient-to-r from-orange-500 to-yellow-300 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
                 <span className="relative text-black group-hover:text-white">
                   Send Message
                 </span>
@@ -109,7 +109,7 @@ const Contact = () => {
         <div>
           <iframe
             className=""
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10432.550414545298!2d91.81833422915041!3d22.41157129675362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd9137c580dbd%3A0xaeb6e95da2a45879!2sTazan%20Group!5e0!3m2!1sen!2sbd!4v1702977971780!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d922.4512217194696!2d91.8140612695338!3d22.360994938988757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd88d0af07d61%3A0xbbf9590c4e77a64d!2sRoad%20No.%201%2C%20Chittagong!5e0!3m2!1sen!2sbd!4v1714885505967!5m2!1sen!2sbd"
             width="600"
             height="350"
             style={{ border: 0 }}
